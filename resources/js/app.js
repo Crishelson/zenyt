12d0e7367ceae4e9f70d38a622e56550231b6db5
Crishelson Oliveira <1luize2luiz@gmail.com>
2026-07-12 17:44:48 -0300

    feat: 🎨 Go ahead to Livewire and start Alpine.js in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-//
+import Alpine from 'alpinejs';
+import { createIcons } from 'lucide';
+
+document.addEventListener('livewire:navigated', () => {
+    createIcons();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    createIcons();
+});
+
+window.Alpine = Alpine;
+
+Alpine.start();
